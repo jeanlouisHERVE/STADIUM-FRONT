1b24d0682a79ce36087334fc18ee0938d6ead8d3
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-10-26 14:48:47 +0200

    creation dashboardsuperadmin

---
+++ src/components/AsideNavbar/index.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+import './styles.scss';
+
+// == Composant
+const AsideNavbar = () => (
+  <div className="AsideNavbar">
+    <h1>AsideNavbar</h1>
+  </div>
+);
+
+// == Export
+export default AsideNavbar;
---
+++ src/pages/DashboardSuperAdmin/index.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+import './styles.scss';
+
+// == Composant
+const DashboardSuperAdmin = () => (
+  <div className="DashboardSuperAdmin">
+    <h1>DashboardSuperAdmin</h1>
+  </div>
+);
+
+// == Export
+export default DashboardSuperAdmin;
