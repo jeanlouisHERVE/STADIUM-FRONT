ee9e124f9877d5b5de19ee1dfaebb0057f7c128d
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-10-27 13:07:18 +0200

    adding icons to listing component

--- src/components/Listing/index.js
+++ src/components/Listing/index.js
@@ -1,4 +1,4 @@
-import './styles.scss';
+import './styles.scss'
 
 // == Composant
 const Listing = () => (
@@ -10,12 +10,37 @@ const Listing = () => (
       </div>
       <div className="listing-header-rightside">
         <div className="listing-filter"><h2>Filtrer</h2></div>
-        <div className="listing-filterIcon"><span className=" material-icons">add</span></div>
+        <div className="listing-filter">
+          <div class="filters__task filters__task--completion buttons are-small has-addons">
+            <button class="filters__choice button">A - Z </button>
+            <button class="filters__choice button">Z - A</button>
+          </div>
+        </div>
       </div>
     </div>
     <div className="listing-corpse">
-
-
+      <table className="listing-table">
+        <tr className="listing-table-header">
+          <th className="listing-table-header-item">Prénom</th>
+          <th className="listing-table-header-item">Nom</th>
+          <th className="listing-table-header-item">Icones</th>
+        </tr>
+        <tr className="listing-table-ligne-container">
+          <div className="listing-table-ligne-leftside">
+            <td>Frédéric</td>
+            <td>Barsoti</td>
+          </div>
+          <td className="listing-table-ligne-rightside">
+            <span class="material-icons">email</span>
+            <span class="material-icons">border_color</span>
+            <span class="material-icons">visibility</span>
+            <span class="material-icons">delete</span>
+          </td>
+        </tr> 
+        <tr><td>Mathilde</td><td>Dupont</td></tr> 
+        <tr><td>Omar</td><td>Malalid</td></tr> 
+        <tr><td>Camille</td><td>Lemaire</td></tr>
+      </table>
     </div>
   </div>
 );
