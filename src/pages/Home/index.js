6feee01ad5756df0e2a2bc44081ed0dbb5744979
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-10-27 13:11:22 +0200

    correction homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
 import './styles.scss';
 import ButtonFormLogin from '../../components/ButtonFormLogin';
-<<<<<<< HEAD
 import ButtonFormSubscribe from '../../components/ButtonFormSubscribe';
 import Footer from '../../components/Footer';
 // == Composant
@@ -78,33 +77,6 @@ const Home = () => (
       <div className="footer">
         <Footer /> 
       </div>
-=======
-
-// == Composant
-const Home = () => (
-  <div className="home">
-    <div className="part-inscription">
-      <h1 className="part-inscription-title"> Stadium est la solution simple, pratique et gratuite au service de la vie associative. </h1>
-      <p className=""> Rejoignez un réseau d’associations sportives, présentes partout en France.
-        Inscrivez-vous pour bénéficiez d’un service de qualité et optimisé
-        pour vous faciliter le quotidien.
-      </p>
-    </div>
-    <div className="part-connexion">
-      <ButtonFormLogin />
-      <HomePicture />
-    </div>
-
-    <div>
-      <div className=""> </div>
-      <div>
-       <DashboardPicture />
-      </div>
-      <div>
-        <p> </p>
-      </div>
-    </div>
->>>>>>> fd2ed07c51859483af53130af89cbcc8a6779fa6
   </div>
 
   
