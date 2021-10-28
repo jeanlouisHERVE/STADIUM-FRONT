5d5e1764e309b0570a4cf2d9cee84300169eecb4
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-10-28 13:57:07 +0200

    add assoData in Utils

--- src/pages/DashboardSuperAdminCommands/index.js
+++ src/pages/DashboardSuperAdminCommands/index.js
@@ -13,7 +13,11 @@ const DashboardSuperAdmin = () => (
     <div className="dashboard-superadmin-rightside">
       <h1 className="dashboard-superadmin-title">Tableau de bord NameOfTheAssociation</h1>
       <div className="dashboard-superadmin-presentation">
-        <h2>Nom prénom</h2>
+        <p className="dashboard-superadmin-presentation-item">PresidentLastname</p>
+        <p className="dashboard-superadmin-presentation-item">PresidentFirstname</p>
+        <p className="dashboard-superadmin-presentation-item">Address</p>
+        <p className="dashboard-superadmin-presentation-item">PhoneNumber</p>
+        <p className="dashboard-superadmin-presentation-item">NumberofAdherents</p>
       </div>
       <div className="dashboard-superadmin-listing-wrapper">
         <Listing adherents={adherentsData} />
