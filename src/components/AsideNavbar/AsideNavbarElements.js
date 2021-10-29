e102a8ef951de2a5b466d15cff596d66e8365ca7
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-10-29 15:52:32 +0200

    asideNavBar responsive

--- src/components/AsideNavbar/AsideNavbarElements.js
+++ src/components/AsideNavbar/AsideNavbarElements.js
@@ -10,7 +10,33 @@ list-style-type: none;
 `;
 
 export const NavLink = styled.div`
-    display: flex;
+    
+    @media (max-width: 1549px) {
+      display: flex;
+    flex-direction: row;
+    align-content: center;
+    width: 90%;
+    color: #ECF5D2;
+    text-transform: uppercase;
+    background-color: #02A5A5;
+    border-top-right-radius: 20px;
+    border-bottom-right-radius: 20px;
+    margin-bottom: 10px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    font-weight: 700;
+    font-size: small;
+    cursor: pointer;
+    height:40px;
+
+    &:hover {
+        background-color: #D87444;
+    }
+  }
+
+    @media (min-width: 1550px) {
+      display: flex;
     flex-direction: row;
     align-content: center;
     width: 75%;
@@ -29,6 +55,8 @@ export const NavLink = styled.div`
 
     &:hover {
         background-color: #D87444;
+  }
+   
 
     }
 `;
