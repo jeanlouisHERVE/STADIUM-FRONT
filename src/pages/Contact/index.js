a0bd7d9a2ba78d748f977515928a468c1ba7db83
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-10-31 17:49:35 +0100

    starting styling ContactForm

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,5 +1,6 @@
 import './styles.scss';
 import ButtonFormLogin from '../../components/ButtonFormLogin';
+import ContactForm from '../../components/ContactForm';
 import Footer from '../../components/Footer';
 
 // == Composant
@@ -16,30 +17,7 @@ const Contact = () => (
         <div className="Contact-rightside-empty" />
         <div className="Contact-rightside-content">
           <h1 className="Contact-title">Contact</h1>
-          <p className="Contact-text">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-            qui officia deserunt mollit anim id est laborum."
-          </p>
-          <p className="Contact-text">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-            qui officia deserunt mollit anim id est laborum."
-          </p>
-          <p className="Contact-text">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-            qui officia deserunt mollit anim id est laborum."
-          </p>
+          <ContactForm />
         </div>
         <div className="Contact-rightside-empty" />
       </div>
