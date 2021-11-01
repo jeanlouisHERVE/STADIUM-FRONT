44947914c2d31270e90b2a1e177591666d61090f
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-11-01 16:34:39 +0100

    creation error404 page

--- src/pages/Error404/index.js
+++ src/pages/Error404/index.js
@@ -1,12 +1,37 @@
-import { Link } from 'react-router-dom';
+
 import './styles.scss';
+import ButtonFormLogin from '../../components/ButtonFormLogin';
+import Footer from '../../components/Footer';
+import Railway from './railway.jpg';
 
 // == Composant
-const Error404 = () => (
-  <div className="Home">
-    <h1>Error404</h1>
+const Contact = () => (
+  <div className="Error-container">
+    <div className="Error-UpPart">
+      <div className="Error-leftside" style={{ backgroundImage: `url(${Railway})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        <div className="Error-leftside-logo">
+          <p>LOGO</p>
+        </div>
+      </div>
+      <div className="Error-rightside">
+        <ButtonFormLogin />
+        <div className="Error-rightside-empty" />
+        <div className="Error-rightside-content">
+          <h1>Erreur 404</h1>
+          <h2>La page que vous recherchez n'existe pas encore...</h2>
+          <input
+            className="button"
+            type="button"
+            value="Accueil"
+            onClick=""
+          />
+        </div>
+        <div className="Error-rightside-empty" />
+      </div>
+    </div>
+    <Footer />
   </div>
 );
 
 // == Export
-export default Error404;
+export default Contact;
