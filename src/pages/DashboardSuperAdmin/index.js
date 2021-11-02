9a214a5f28b6a49182d625fe0fab966ff49b9a5d
Pablo Cany <pablo.cany@live.fr>
2021-11-03 00:17:09 +0100

    fix CSS issues on homePage + navbar responsive for Dashboard

--- src/pages/DashboardSuperAdmin/index.js
+++ src/pages/DashboardSuperAdmin/index.js
@@ -1,20 +1,35 @@
+import { useState } from 'react';
 import AsideNavbar from '../../components/AsideNavbar';
+import Navbar from '../../components/Navbar';
+import Sidebar from '../../components/Sidebar';
 import CardAssociation from '../../components/CardAssociation';
 
 import './styles.scss';
 import associationDatas from './data/associations';
 
 // == Composant
-const DashboardSuperAdmin = () => (
-  <div className="dashboard-superadmin">
-    <AsideNavbar />
-    <div className="cards">
-      {associationDatas.map((association) => (
-        <CardAssociation key={association.id} association={association} />
-      ))}
-    </div>
-  </div>
-);
+const DashboardSuperAdmin = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <div className="dashboard-superadmin">
+        <AsideNavbar />
+        <div className="cards">
+          {associationDatas.map((association) => (
+            <CardAssociation key={association.id} association={association} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
 // == Export
 export default DashboardSuperAdmin;
