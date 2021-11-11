35bbaf0e0f1d73f47f3df6422fa0713ca0767023
Stephanie-M <stephanie.martial@live.fr>
2021-11-11 23:54:33 +0100

    scss style redesign done- redesign html- progress on responsive

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,84 +14,81 @@ import './styles.scss';
 // == Composant
 const Home = () => (
   <>
-    <div className="home">
-      <div className="home-header-header">
-        <div className="part-registration-logo">
-          <Link to="/">
-            <img src={imageLogo} alt="" className="imageLogo" />
-          </Link>
-        </div>
-        <div className="part-connexion-btn">
-          <Link to="/connexion"> <ButtonFormLogin /> </Link>
-        </div>
-      </div>
-      <div className="home-content">
-        <div className="home-header">
-          <div className="part-registration">
-            <div className="part-registration-content">
-              <h1 className="part-registration-title"> Stadium est la solution simple, pratique et gratuite au service de la vie associative. </h1>
-              <p className="part-registration-text">
-                Rejoignez un réseau d'associations sportives, présentes partout en France.<br />
-                Inscrivez-vous pour bénéficiez d'un service de qualité et optimisé pour vous
-                faciliter le quotidien.
-              </p>
-              <div className="part-registration-btn-subscribe">
-                <Link to="/inscription"> <ButtonFormSubscribe /> </Link>
-              </div>
-            </div>
-          </div>
-          <div className="part-connexion">
+    <div className="home"> {/** ok */}
 
-            <img src={imageStade} alt="" className="stadium-img" />
+      <div className="home-content">{/** ok */}
+        <div className="part-registration">{/** ok */}
+          <h1 className="part-registration-title"> Stadium est la solution simple, pratique et gratuite au service de la vie associative. </h1>
+          <p className="part-registration-text">
+            Rejoignez un réseau d'associations sportives, présentes partout en France.<br />
+            Inscrivez-vous pour bénéficiez d'un service de qualité et optimisé pour vous
+            faciliter le quotidien.
+          </p>
+          <div className="part-registration-btn-subscribe">
+            <Link to="/inscription"> <ButtonFormSubscribe /> </Link>
           </div>
-        </div>
-        <div className="part-association">
-          <div className="part-association-title">
-            <h1 className="part-association-title-title"> <strong> Vous êtes une association? </strong> <br /> Stadium est la solution simple, pratique et gratuite au service de la vie associative. </h1>
-          </div>
-          <div className="part-association-content">
-            <div className="part-association-content-content">
-              <p className="part-association-text"> Stadium vous permet de dématérialiser entièrement la gestion de vos contrats d'adhérents.</p>
-              <p className="part-association-text"> Opter pour Stadium, c'est choisir de se simplifier la vie face aux tâches administratives et chronophages que représente la gestion de centaines voir de milliers de contrats. </p>
-              <p className="part-association-text"> Notre plateforme est un outil qui vous permet de réaliser un panel de tâches de façon simple et intuitive : </p>
-              <ul>
-                <li className="part-association-text-elem-list"> gestion des membres,</li>
-                <li className="part-association-text-elem-list"> adhésion en ligne,</li>
-                <li className="part-association-text-elem-list"> gestion du planning de cours,</li>
-                <li className="part-association-text-elem-list"> gestion de la communication sur vos évènements,</li>
-                <li className="part-association-text-elem-list"> site internet </li>
-              </ul>
-              <p className="part-association-text"> Stadium permet aux associations de gagner du temps dans leur gestion quotidienne afin de se concentrer sur le développement de leur activité. </p>
-              <p className="part-association-text"> L’inscription sur la plateforme permet automatiquement à une association d’avoir un site internet accessible et visible par tous les membres de Stadium.</p>
-              <p className="part-association-text"> Stadium réunit sur sa plateforme tant les associations que les personnes en recherche d’une activité sportive. De quoi gagner en visibilité et se faire connaître. </p>
-            </div>
-            <div className="part-association-img">
-              <img src={dashboardUser} alt="" className="dashboard-association-img" />
-            </div>
-            <div className="part-association-btn-subscribe"> <Link to="/inscription"> <ButtonFormSubscribe /> </Link> </div>
-          </div>
-        </div>
-        <div className="part-user">
-          <div className="part-user-title">
-            <h1 className="part-user-title-title"> <strong> Vous êtes un adhérent?</strong> <br /> Stadium est la solution simple, pratique et gratuite pour suivre votre association et vos activités. </h1>
-          </div>
-          <div className="part-user-content">
-            <div className="part-user-content-content">
-              <p className="part-user-text"> Inscrivez-vous sur Stadium et : </p>
-              <ul>
-                <li className="part-user-text-elem-list"> Retrouvez facilement les associations sportives autour de chez vous,</li>
-                <li className="part-user-text-elem-list">Inscrivez-vous à vos activités favorites depuis votre domicile, </li>
-                <li className="part-user-text-elem-list"> Gérer vos contrats à distance,</li>
-                <li className="part-user-text-elem-list"> Renseignez-vous et inscrivez-vous aux évènements qui vous intéressent en un clic.</li>
-              </ul>
-            </div>
-            <div className="part-user-img">
-              <img src={dashboardUser} alt="" className="dashboard-user-img" />
-            </div>
-            <div className="part-user-btn-subscribe"> <Link to="/inscription"> <ButtonFormSubscribe /> </Link> </div>
-          </div>
-        </div>
-      </div>
+        </div>{/** ok */}
+
+        <div className="part-connexion">{/** ok */}
+          <img src={imageStade} alt="" className="stadium-img" />
+        </div>{/** ok */}
+        <div className="home-header-header">{/** ok */}
+          <div className="part-registration-logo">{/** ok */}
+            <Link to="/">
+              <img src={imageLogo} alt="" className="imageLogo" />
+            </Link>
+          </div>{/** ok */}
+          <div className="part-connexion-btn">{/** ok */}
+            <Link to="/connexion"> <ButtonFormLogin /> </Link>
+          </div>{/** ok */}
+        </div>{/** ok */}
+      </div>{/** ok */}
+      <div className="part-association"> {/** ok */}
+        <div className="part-association-title"> {/** ok */}
+          <h1 className="part-association-title-title"> <strong> Vous êtes une association? </strong> <br /> Stadium est la solution simple, pratique et gratuite au service de la vie associative. </h1>
+        </div> {/** ok */}
+        <div className="part-association-content"> {/** ok */}
+          <div className="part-association-content-content"> {/** ok */}
+            <p className="part-association-text"> Stadium vous permet de dématérialiser entièrement la gestion de vos contrats d'adhérents.</p>
+            <p className="part-association-text"> Opter pour Stadium, c'est choisir de se simplifier la vie face aux tâches administratives et chronophages que représente la gestion de centaines voir de milliers de contrats. </p>
+            <p className="part-association-text"> Notre plateforme est un outil qui vous permet de réaliser un panel de tâches de façon simple et intuitive : </p>
+            <ul>
+              <li className="part-association-text-elem-list"> gestion des membres,</li>
+              <li className="part-association-text-elem-list"> adhésion en ligne,</li>
+              <li className="part-association-text-elem-list"> gestion du planning de cours,</li>
+              <li className="part-association-text-elem-list"> gestion de la communication sur vos évènements,</li>
+              <li className="part-association-text-elem-list"> site internet </li>
+            </ul>
+            <p className="part-association-text"> Stadium permet aux associations de gagner du temps dans leur gestion quotidienne afin de se concentrer sur le développement de leur activité. </p>
+            <p className="part-association-text"> L’inscription sur la plateforme permet automatiquement à une association d’avoir un site internet accessible et visible par tous les membres de Stadium.</p>
+            <p className="part-association-text"> Stadium réunit sur sa plateforme tant les associations que les personnes en recherche d’une activité sportive. De quoi gagner en visibilité et se faire connaître. </p>
+          </div>{/** ok */}
+          <div className="part-association-img">{/** ok */}
+            <img src={dashboardUser} alt="" className="dashboard-association-img" />
+          </div>{/** ok */}
+          <div className="part-association-btn-subscribe"> <Link to="/inscription"> <ButtonFormSubscribe /> </Link> </div>
+        </div>{/** ok */}
+      </div>{/** ok */}
+      <div className="part-user">{/** ok */}
+        <div className="part-user-title">{/** ok */}
+          <h1 className="part-user-title-title"> <strong> Vous êtes un adhérent?</strong> <br /> Stadium est la solution simple, pratique et gratuite pour suivre votre association et vos activités. </h1>
+        </div>{/** ok */}
+        <div className="part-user-content">{/** ok */}
+          <div className="part-user-content-content">{/** ok */}
+            <p className="part-user-text"> Inscrivez-vous sur Stadium et : </p>
+            <ul>
+              <li className="part-user-text-elem-list"> Retrouvez facilement les associations sportives autour de chez vous,</li>
+              <li className="part-user-text-elem-list">Inscrivez-vous à vos activités favorites depuis votre domicile, </li>
+              <li className="part-user-text-elem-list"> Gérer vos contrats à distance,</li>
+              <li className="part-user-text-elem-list"> Renseignez-vous et inscrivez-vous aux évènements qui vous intéressent en un clic.</li>
+            </ul>
+          </div>{/** ok */}
+          <div className="part-user-img">{/** ok */}
+            <img src={dashboardUser} alt="" className="dashboard-user-img" />
+          </div>{/** ok */}
+          <div className="part-user-btn-subscribe"> <Link to="/inscription"> <ButtonFormSubscribe /> </Link> </div>
+        </div>{/** ok */}
+      </div>{/** ok */}
     </div>
     <div className="footer">
       <Footer />
