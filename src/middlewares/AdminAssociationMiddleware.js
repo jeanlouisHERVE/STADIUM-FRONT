38d2429161d19086fa0e64ff6e5e2173b02b2e3f
Pablo Cany <pablo.cany@live.fr>
2021-11-16 13:51:19 +0100

    user authentified allowed with true in state while doing admin dashboard

--- src/middlewares/AdminAssociationMiddleware.js
+++ src/middlewares/AdminAssociationMiddleware.js
@@ -10,7 +10,7 @@ const superAdminMiddleware = (store) => (next) => (action) => {
 
       // axios.get(`https://sym-stadium.herokuapp.com/api/v1${path.pathname}`)
       // axios.get('https://sym-stadium.herokuapp.com/api/v1/backoffice/superadmin/associations/')
-      // axios.get('http://ec2-54-197-70-206.compute-1.amazonaws.com/api/v1/backoffice/superadmin/associations/')
+      axios.get('http://ec2-54-197-70-206.compute-1.amazonaws.com/api/v1/backoffice/admin/association/')
         .then((response) => {
           // console.log(response);
 
